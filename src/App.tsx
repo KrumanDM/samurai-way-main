fd10e01c180c4acfad505d8cee684f7bcfd1eafc
KrumanDM <120024603+KrumanDM@users.noreply.github.com>
2023-03-03 18:59:08 +0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,55 @@
-import { OmitProps } from "antd/lib/transfer/ListBody";
-import { type } from "os";
+
 import React from "react";
 import "./App.css";
-import Footer from "./Footer";
-import Header from "./Header";
-import Technologies from "./Technologies";
+
 
 
 function App() {
   return (
-    <div>
-      <Header title={"Programers"} />
-      <Technologies title={"cssssss"}/>
-      <Footer value={1999}/>
-      <Jankins value={2007}/>
-    </div>
-  );
-}
-
-
-
-type JankinsPropsType = {
-  value: number
-}
-
-
-
-const Jankins = (props: JankinsPropsType) => {
-  return (
-    <div>
-      <a>{props.value}</a>
-    </div>
-  )
+    <div className="app-wrapper">
+      <header className="header">
+      <img src="#"></img>
+      </header>
+      <nav className="nav">
+      <div>
+        <a>Profile</a> 
+      </div>
+      <div>
+        <a>Messages</a> 
+      </div>
+      <div>
+        <a>News</a> 
+      </div>
+      <div>
+        <a>Music</a> 
+      </div>
+      <div>
+        <a>Settinghs</a> 
+      </div>
+      </nav>
+      <div className="content">
+        <div>
+        <img src="https://sun2.cosmostv-by-minsk.userapi.com/impg/O_Sfh3gmJlbUBh-2hgwvkh-bXeRGMyV0QHLJ_Q/DJAb9B1CIGA.jpg?size=604x364&quality=96&sign=0c3e2a12e7302055b558287a7f111fe8&type=album"></img>
+        </div>
+        <div>
+          ava + description
+        </div>
+        <div>
+          My posts
+          <div>
+            New post
+          </div>
+          <div>
+            <div>
+              post 1
+            </div>
+            <div>
+              post 2
+            </div>
+          </div>
+        </div>
+      </div> 
+    </div>);
 }
 
 
