2ca5461fd9ac956bf3d98a1d302c8de951732fd6
KrumanDM <120024603+KrumanDM@users.noreply.github.com>
2023-03-03 19:11:19 +0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
 
 
 
@@ -11,28 +12,7 @@ function App() {
     <div className="app-wrapper">
       <Header />
       <Navbar />
-      <div className="content">
-        <div>
-        <img src="https://sun2.cosmostv-by-minsk.userapi.com/impg/O_Sfh3gmJlbUBh-2hgwvkh-bXeRGMyV0QHLJ_Q/DJAb9B1CIGA.jpg?size=604x364&quality=96&sign=0c3e2a12e7302055b558287a7f111fe8&type=album"></img>
-        </div>
-        <div>
-          ava + description
-        </div>
-        <div>
-          My posts
-          <div>
-            New post
-          </div>
-          <div>
-            <div>
-              post 1
-            </div>
-            <div>
-              post 2
-            </div>
-          </div>
-        </div>
-      </div> 
+      <Profile />
     </div>);
 }
 
---
+++ src/components/Profile.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Profile = () => {
+    return(
+      <div className="content">
+      <div>
+      <img src="https://sun2.cosmostv-by-minsk.userapi.com/impg/O_Sfh3gmJlbUBh-2hgwvkh-bXeRGMyV0QHLJ_Q/DJAb9B1CIGA.jpg?size=604x364&quality=96&sign=0c3e2a12e7302055b558287a7f111fe8&type=album"></img>
+      </div>
+      <div>
+        ava + description
+      </div>
+      <div>
+        My posts
+        <div>
+          New post
+        </div>
+        <div>
+          <div>
+            post 1
+          </div>
+          <div>
+            post 2
+          </div>
+        </div>
+      </div>
+    </div>
+    )
+}
+
+export default Profile
