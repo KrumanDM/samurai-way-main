a0357a7b54c3e4be7af1604d2b270740490ab44d
KrumanDM <120024603+KrumanDM@users.noreply.github.com>
2023-03-05 11:56:54 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,14 @@ import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import Profile from "./components/Profile";
 
-
-
 function App() {
   return (
     <div className="app-wrapper">
       <Header />
       <Navbar />
       <Profile />
-    </div>);
+    </div>
+  );
 }
-
 
 export default App;
