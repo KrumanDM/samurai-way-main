66cf2bbfe7925ddad8e482d4378a50e5473f00e1
KrumanDM <120024603+KrumanDM@users.noreply.github.com>
2023-03-10 14:08:22 +0300

    upd

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,23 @@
 import React from "react";
+import s from  "./Navbar.module.css"
 
 
 const Navbar = () => {
   return (
-    <nav className="nav">
-      <div className="item">
+    <nav className={s.nav}>
+      <div className={s.item}>
         <a>Profile</a>
       </div>
-      <div className="item">
+      <div className={s.item}>
         <a>Messages</a>
       </div>
-      <div className="item">
+      <div className={s.item}>
         <a>News</a>
       </div>
-      <div className="item">
+      <div className={s.item}>
         <a>Music</a>
       </div>
-      <div className="item">
+      <div className={s.item}>
         <a>Settinghs</a>
       </div>
     </nav>
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import s from"./Profile.module.css"
 
 const Profile = () => {
   return (
-    <div className="content">
+    <div className={s.content}>
       <div>
         <img src="https://sun2.cosmostv-by-minsk.userapi.com/impg/O_Sfh3gmJlbUBh-2hgwvkh-bXeRGMyV0QHLJ_Q/DJAb9B1CIGA.jpg?size=604x364&quality=96&sign=0c3e2a12e7302055b558287a7f111fe8&type=album"></img>
       </div>
@@ -10,8 +11,8 @@ const Profile = () => {
       <div>
         My posts
         <div>New post</div>
-        <div>
-          <div>post 1</div>
+        <div className={s.posts}>
+          <div className={s.item}>post 1</div>
           <div>post 2</div>
         </div>
       </div>
