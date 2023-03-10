400ae0c00b9702bcce0a1eeaf14c8b4163a7577b
KrumanDM <120024603+KrumanDM@users.noreply.github.com>
2023-03-10 14:14:49 +0300

    upd

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,8 +4,8 @@ import s from  './Header.module.css'
 
 const Header = () => {
   return (
-    <header className="header">
-      <img src="#"></img>
+    <header className={s.header}>
+      <img src="https://i.gifer.com/origin/24/24a4514b19708aacfd2c816a75b3fe79_w200.gif"></img>
     </header>
   );
 };
