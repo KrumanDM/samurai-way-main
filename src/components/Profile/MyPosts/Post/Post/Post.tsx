34ae4bedeb4b224713c319fa98f093d067737ef7
KrumanDM <120024603+KrumanDM@users.noreply.github.com>
2023-03-10 16:47:57 +0300

    upd

---
+++ src/components/Profile/MyPosts/Post/Post/Post.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import s from"./Post.module.css"
+
+const Post = () => {
+  return (
+    <div className={s.item}>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRy81hYTjh2crLdaYkR1irTcMHtqLJ105VvN6pJn9vKW0mXxMCg_VbvXBnqAfrMAQCxDYw&usqp=CAU" />
+      post 1
+      <div>
+      <span>like</span>
+      </div>
+    </div>
+  );
+};
+
+
+export default Post;
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import MyPosts from "./MyPosts/Post/MyPosts";
 import s from"./Profile.module.css"
 
 const Profile = () => {
@@ -8,14 +9,7 @@ const Profile = () => {
         <img src="https://sun2.cosmostv-by-minsk.userapi.com/impg/O_Sfh3gmJlbUBh-2hgwvkh-bXeRGMyV0QHLJ_Q/DJAb9B1CIGA.jpg?size=604x364&quality=96&sign=0c3e2a12e7302055b558287a7f111fe8&type=album"></img>
       </div>
       <div>ava + description</div>
-      <div>
-        My posts
-        <div>New post</div>
-        <div className={s.posts}>
-          <div className={s.item}>post 1</div>
-          <div>post 2</div>
-        </div>
-      </div>
+      <MyPosts />
     </div>
   );
 };
