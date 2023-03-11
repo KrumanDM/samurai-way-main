074e39c198e77225496ab228127038a97d39f572
KrumanDM <120024603+KrumanDM@users.noreply.github.com>
2023-03-11 16:15:59 +0300

    upd

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 
 import React from "react";
 import "./App.css";
+import Dialogs from "./components/Dialogs/Dialogs";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <Navbar />
       <Profile />
+      <Dialogs />
     </div>
   );
 }
