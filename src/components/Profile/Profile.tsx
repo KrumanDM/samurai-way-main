74267856c600c49192a183cbbc3860e2bd5f6d93
KrumanDM <120024603+KrumanDM@users.noreply.github.com>
2023-03-18 23:39:22 +0300

    Update Profile.tsx

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -10,7 +10,7 @@ const Profile: React.FC<MessageType> = (props) => {
       <div>
         <img src="https://sun2.cosmostv-by-minsk.userapi.com/impg/O_Sfh3gmJlbUBh-2hgwvkh-bXeRGMyV0QHLJ_Q/DJAb9B1CIGA.jpg?size=604x364&quality=96&sign=0c3e2a12e7302055b558287a7f111fe8&type=album"></img>
       </div>
-      <div>ava  +  description</div>
+      <div>ava +  description</div>
       <MyPosts />
     </div>
   );
