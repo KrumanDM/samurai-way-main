d82b2f0045cb71042bbcf560f0dfceb2a40c2118
KrumanDM <120024603+KrumanDM@users.noreply.github.com>
2023-03-18 23:35:42 +0300

    Update Post.tsx

--- src/components/Profile/MyPosts/Post/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post/Post.tsx
@@ -5,7 +5,7 @@ const Post = () => {
   return (
     <div className={s.item}>
       <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRy81hYTjh2crLdaYkR1irTcMHtqLJ105VvN6pJn9vKW0mXxMCg_VbvXBnqAfrMAQCxDYw&usqp=CAU" />
-       post 1
+        post 1
       <div>
         <span> like </span>
       </div>
