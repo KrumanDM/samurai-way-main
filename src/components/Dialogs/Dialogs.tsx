09020448f061e57e13211d4a35617e93f58002f4
KrumanDM <120024603+KrumanDM@users.noreply.github.com>
2023-04-11 12:53:12 +0300

    Update Dialogs.tsx

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -4,28 +4,31 @@ import {NavLink} from "react-router-dom"
 
 type MessageType = any;
 
+type DialogItemType = {
+  name: string
+  id: string
+}
+
+const DialogItem = (props:DialogItemType) =>{
+  let path = "/dialogs/1" + props.id
+
+  return <div className={s.dialog + " " + s.active}>
+  <NavLink to={path}>{props.name}</NavLink> 
+</div>
+}
+
 const Dialogs: React.FC<MessageType> = (props) => {
   return (
     <div className={s.dialogs}>
       <div className={s.dialogsItems}>
-        <div className={s.dialog + " " + s.active}>
-          <NavLink to="/dialogs/1">Dima</NavLink> 
-        </div>
-        <div className={s.dialog}>
-           <NavLink to="/dialogs/2">Artem</NavLink>
-        </div>
-        <div className={s.dialog}>
-            <NavLink to="/dialogs/3">Zhenya</NavLink>
-        </div>
-        <div className={s.dialog}>
-            <NavLink to="/dialogs/4">Valera</NavLink>
-        </div>
-        <div className={s.dialog}>
-            <NavLink to="/dialogs/5">Denis</NavLink>
-        </div>
-        <div className={s.dialog}>
-            <NavLink to="/dialogs/6">Kate</NavLink>
-        </div>
+        
+        <DialogItem name="Dima" id="1"/>
+        <DialogItem name="Artem" id="2"/>
+        <DialogItem name="Zhenya" id="3"/>
+        <DialogItem name="Valera" id="4"/>
+        <DialogItem name="Denis" id="5"/>
+        <DialogItem name="Kate" id="6"/>
+        
       </div>
       <div className={s.messages}>
         <div className={s.dialog}>Hi</div>
