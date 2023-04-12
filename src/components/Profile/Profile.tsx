5ec777ad1bf5218303c3f29c5d83cbb9ab4a2908
KrumanDM <120024603+KrumanDM@users.noreply.github.com>
2023-04-12 12:21:57 +0300

    .

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,16 +1,15 @@
 import React from "react";
 import MyPosts from "./MyPosts/Post/MyPosts";
 import s from"./Profile.module.css"
+import ProfileInfo from "./ProfileInfo/ProfileInfo";
 
 type MessageType = (any)
 
 const Profile: React.FC<MessageType> = (props) => {
   return (
     <div >
-      <div>
-        <img src="https://sun2.cosmostv-by-minsk.userapi.com/impg/O_Sfh3gmJlbUBh-2hgwvkh-bXeRGMyV0QHLJ_Q/DJAb9B1CIGA.jpg?size=604x364&quality=96&sign=0c3e2a12e7302055b558287a7f111fe8&type=album"></img>
-      </div>
-      <div>ava + description </div>
+      <ProfileInfo />
+      
       <MyPosts />
     </div>
   );
---
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import s from"./ProfileInfo.module.css"
+
+type MessageType = (any)
+
+const ProfileInfo: React.FC<MessageType> = (props) => {
+  return (
+    <div >
+      <div>
+        <img src="https://sun2.cosmostv-by-minsk.userapi.com/impg/O_Sfh3gmJlbUBh-2hgwvkh-bXeRGMyV0QHLJ_Q/DJAb9B1CIGA.jpg?size=604x364&quality=96&sign=0c3e2a12e7302055b558287a7f111fe8&type=album"></img>
+      </div>
+      <div>ava + description </div>
+      {/* <MyPosts /> */}
+    </div>
+  );
+};
+
+export default ProfileInfo;
