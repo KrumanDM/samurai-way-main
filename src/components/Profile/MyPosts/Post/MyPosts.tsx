32c34fdff43a0ae4b62ad4eb2f84019654ab527b
KrumanDM <120024603+KrumanDM@users.noreply.github.com>
2023-04-12 12:28:25 +0300

    .

--- src/components/Profile/MyPosts/Post/MyPosts.tsx
+++ src/components/Profile/MyPosts/Post/MyPosts.tsx
@@ -4,11 +4,15 @@ import Post from "./Post/Post";
 
 const MyPosts = () => {
   return (
-    <div>
+    <div className={s.postsBlock}>
            My posts
       <div>
+        <div>
         <textarea></textarea>
+        </div>
+        <div>
         <button>  Add post </button>
+        </div>
       </div>
       <div className={s.posts}>
         <Post />
