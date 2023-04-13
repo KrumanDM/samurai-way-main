bd632f17eaba40ee0c3bbe13146d49d3b24fc614
KrumanDM <120024603+KrumanDM@users.noreply.github.com>
2023-04-13 22:29:09 +0300

    Update Dialogs.tsx

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -41,29 +41,20 @@ const Dialogs: React.FC<MessageType> = (props) => {
     {id:"3",message:"Really well"},
     
   ]
-  
-  let dialogsElement = dialogs.map(dialog => {<DialogItem name={dialog.name} id={dialog.id}/>})
 
-  
-  let messagesElement = messages.map(m => {<Message message={m.message}/>})
+  let dialogsElement = dialogs.map(d => <DialogItem name={d.name} id={d.id}/>)
+  let messagesElement = messages.map(m => <Message message={m.message}/>)
 
   return (
     <div className={s.dialogs}>
       <div className={s.dialogsItems}>
 
-        {/* <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
-        <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
-        <DialogItem name="Zhenya" id="3"/>
-        <DialogItem name="Valera" id="4"/>
-        <DialogItem name="Denis" id="5"/>
-        <DialogItem name="Kate" id="6"/> */}
+        {dialogsElement}
         
       </div>
       <div className={s.messages}>
-        {/* <Message message={messagesData[0].message}/>
-        <Message message={messagesData[1].message}/>
-        <Message message={messagesData[2].message}/> */}
-      
+        
+      {messagesElement}
         
       </div>
     </div>
