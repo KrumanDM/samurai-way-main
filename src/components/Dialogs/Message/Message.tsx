5a692d19b59b788a6284c6749c5c85a4e51498a3
KrumanDM <120024603+KrumanDM@users.noreply.github.com>
2023-04-15 12:49:20 +0300

    Update Message.tsx

---
+++ src/components/Dialogs/Message/Message.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import s from './Dialogs.module.css'
+import { NavLink } from "react-router-dom";
+
+type MessageTypeProps = any
+
+const Message = (props:MessageTypeProps) => {
+    return <div className={s.dialog}>{props.message}</div>
+  }
